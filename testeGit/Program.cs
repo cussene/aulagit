fa9eb7d7927be5fd648d8b79f009f3e8ed97beba
cussene <cussene93@gmail.com>
2021-08-20 21:47:09 +0200

    Projecto iniciado mostrando a mensagem Ola mundo

---
+++ testeGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testeGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
